61df0205bd041ecd07f2e06d9dcf742d3b560cb8
tamarastrikalkina <tstrikalkina@gmail.com>
2026-09-09 13:44:17 +0200

    new progect

---
+++ utils/config.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://contactapp-telran-backend.herokuapp.com"
+API_VERSION = "/v1"
+REGISTRATION_URL =  "/user/registration/usernamepassword"
+LOGIN_URL = "/user/login/usernamepassword"
---
+++ utils/conftest.py
@@ -0,0 +1,19 @@
+
+import pytest
+import requests
+from config import *
+
+
+@pytest.fixture (scope="session")
+def registration_url():
+    return BASE_URL+API_VERSION+REGISTRATION_URL
+
+@pytest.fixture (scope="session")
+def login_url():
+    return BASE_URL+API_VERSION+LOGIN_URL
+
+@pytest.fixture (scope="session")
+def session():
+    s = requests.Session()
+    yield s
+    s.close()
